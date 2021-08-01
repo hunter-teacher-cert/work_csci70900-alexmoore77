ae381848363e402ea47094c3e384f69e53afe8ab
Alexander Moore <77898412+alexmoore77@users.noreply.github.com>
2021-08-01 16:00:40 +0000

    Completed Knight's Tour project successfully.  Tried to add a feature to track and display the recursive calls to solve() on the stack.

--- ds/maze/Driver.java
+++ ds/maze/Driver.java
@@ -5,15 +5,30 @@ public class Driver{
 
     public static void main(String[] args) {
 	boolean solved;
-	
+	/*
 	  Maze m = new Maze("maze.dat");
 	solved = m.solve(1,1);
+
 	
-	/*
-	  Knights k - new Knights(5);
+	  Knights k = new Knights(5);
 	  solved = k.solve(1,1);
-	*/
-	//Knights k = new Knights(5);
-	//k.solve(2,2,1);
+		*/
+	
+  System.out.println("*********************************");
+  System.out.println("          Knight's Tour");
+  System.out.println("          By Alex Moore");
+  System.out.println("          Summer 2021");
+  System.out.println("*********************************\n\n");
+
+  
+
+  Knights k = new Knights(5);
+	k.solve(2,2,1);
+//System.out.println(k);
+  
+/*  
+  Knights k6 = new Knights(6);
+	k6.solve(2,2,1);
+ */ 
     }
 }
--- ds/maze/Knights.java
+++ ds/maze/Knights.java
@@ -1,3 +1,12 @@
+/******************************************
+Knight's Tour 
+By Alexander Moore
+For Hunter College Summer 2021
+Description:  The program calls the solve method recursively until it finds a way for the knight to visit every square on a board of the specified size exactly once.
+Accomplishments:  I hope to create a simple chess game at some point in my life, so this has gotten me started.  It has also given me a stronger understanding of recursion in practice.  I actually found this program to be easier and more intuitive than the maze program, perhaps because I love chess.  
+Challenges:  Tried to add instance variable to count the number of recursive calls on the stack and print along with the board, but was not working as intended, so I walked away.  The toString() method slowed down the program too much, so I set it to be called near the end. Did not determine how to call it exactly once at the very end. 
+******************************************/
+
 import java.io.*;
 import java.util.*;
 
@@ -11,6 +20,7 @@ public class Knights{
     private int rows = 5;
     private int cols = 5;
     private int size=5;
+    public int recursiveCallsOnStack=0;
     private String clearScreen="[0;0H\n";
 
     private void delay(int n)
@@ -28,6 +38,7 @@ public class Knights{
 	cols = size;
 	this.size=size;
 	int row,col;
+  recursiveCallsOnStack=0;
 
 	// Notice that the board is 4 bigger in both directions with 0's
 	// in the middle and a 2 row/col border of -1.
@@ -64,13 +75,14 @@ public class Knights{
 		}
 	    }
 	    result = result +"\n";
-	}
+	};
+  result+="Recursive Calls on Stack:"+recursiveCallsOnStack+"\n";
 	return result;
     }
 
     public boolean solve(int col,int row, int count){
 	boolean solved = false;
-
+recursiveCallsOnStack++;
 
 	// This should return true when we've solved the problem
 	// What should CHANGETHIS be?
@@ -78,8 +90,16 @@ public class Knights{
 	// here, when do we know when we're done?
 	// HINT: you have an nxn board and are done when you've visited
 	// every board location
-	if (count>64){
-	    System.out.println(this);
+	
+  //System.out.println("this.size*this.size="+this.size*this.size);
+
+  if (count>((this.size*this.size)))
+  {
+
+//Use only printing at the end to improve efficiency
+	System.out.println(clearScreen+this);
+
+recursiveCallsOnStack--;
 	    return true;
 	}
 
@@ -88,17 +108,31 @@ public class Knights{
 	// change CHANGETHIS to the appropriate boolean
 	// HINT: we are tracking our moves in the board
 	// and also built that border of -1 values.
-	if (board[col][row]){
-	    return false;
+	if (board[col][row]!=0){
+	    
+      recursiveCallsOnStack--;
+      return false;
 	}
 	
-	
+
 	// what do we put into the board
 	// Change CHANGETHIS
-	board[col][row]=CHANGETHIS;
+	board[col][row]=count;
 
-	delay(50);
-	System.out.println(clearScreen+this);
+//location of count++ could be important
+count++;
+
+//print the board only once at the end
+/*
+if (count==(this.size*this.size))
+System.out.println(this);
+*/
+//Cut delay to improve efficiency
+
+//delay(1);
+//Use only one print at the end to improve efficiency
+
+//	System.out.println(clearScreen+this);
 
 
 	// Here we need to do try to do the 8 recursive calls
@@ -108,13 +142,28 @@ public class Knights{
 	// 2. The parameters for the call are a little different.
 	// Add the recursive calls here
 	
+  solve( col+1,row-2 , count);
 
-	
+	solve(col+2,row-1, count);
 
+	solve(col+1,row+2, count);
+
+	solve(col+2,row+1, count);
+
+	solve(col-1,row-2, count);
+
+	solve(col-2,row-1, count);
+
+	solve(col-1,row+2, count);
+
+	solve(col-2,row+1, count);
+  
 	// Here we unset where we were for the backtracking
 	
 	board[col][row]=0;
-	return solved;
+	
+  recursiveCallsOnStack--;
+  return solved;
     }
 
     
